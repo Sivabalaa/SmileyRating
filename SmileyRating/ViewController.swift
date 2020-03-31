ac74e9b6d9ee7ef4b23c809794d00dd80ef61e6d
Sivabalaa <sivabalaajothibose@gmail.com>
2020-03-31 20:09:29 +0530

    Version 1.0

--- SmileyRating/ViewController.swift
+++ SmileyRating/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Rating
+    @IBOutlet weak var sliderStepRating: SliderStep!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,30 +21,29 @@ class ViewController: UIViewController {
     
     //Design
     func setupRatingView() {
-        let sliderStep: SliderStep = SliderStep()
-        sliderStep.stepImages =   [UIImage(named:"terrible")!, UIImage(named:"bad")!, UIImage(named:"okay")!, UIImage(named:"good")!,UIImage(named:"great")!, ]
+        sliderStepRating.stepImages =   [UIImage(named:"terrible")!, UIImage(named:"bad")!, UIImage(named:"okay")!, UIImage(named:"good")!,UIImage(named:"great")!, ]
         
-        sliderStep.tickTitles = ["Terrible", "Bad", "Okay", "Good", "Great"]
-        sliderStep.tickImages = [UIImage(named:"unTerrible")!, UIImage(named:"unBad")!, UIImage(named:"unOkay")!, UIImage(named:"unGood")!,UIImage(named:"unGreat")!, ]
-        sliderStep.minimumValue = 1
-        sliderStep.maximumValue = Float(sliderStep.stepImages!.count) + sliderStep.minimumValue - 1.0
-        sliderStep.stepTickColor = UIColor.clear
-        sliderStep.stepTickWidth = 40
-        sliderStep.stepTickHeight = 40
-        sliderStep.trackHeight = 2
-        sliderStep.value = 5
-        sliderStep.trackColor = #colorLiteral(red: 0.9371728301, green: 0.9373074174, blue: 0.9371433258, alpha: 1)
-        sliderStep.enableTap = true
-        sliderStep.sliderStepDelegate = self
-        sliderStep.isContinuous = true
-        sliderStep.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(sliderStep)
+        sliderStepRating.tickTitles = ["Terrible", "Bad", "Okay", "Good", "Great"]
+        sliderStepRating.tickImages = [UIImage(named:"unTerrible")!, UIImage(named:"unBad")!, UIImage(named:"unOkay")!, UIImage(named:"unGood")!,UIImage(named:"unGreat")!, ]
+        
+        sliderStepRating.minimumValue = 1
+        sliderStepRating.maximumValue = Float(sliderStepRating.stepImages!.count) + sliderStepRating.minimumValue - 1.0
+        sliderStepRating.stepTickColor = UIColor.clear
+        sliderStepRating.stepTickWidth = 40
+        sliderStepRating.stepTickHeight = 40
+        sliderStepRating.trackHeight = 2
+        sliderStepRating.value = 5
+        sliderStepRating.trackColor = #colorLiteral(red: 0.9371728301, green: 0.9373074174, blue: 0.9371433258, alpha: 1)
+        sliderStepRating.enableTap = true
+        sliderStepRating.sliderStepDelegate = self
+        sliderStepRating.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(sliderStepRating)
         NSLayoutConstraint.activate([
-            //sliderStep
-            sliderStep.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 0),
-            sliderStep.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            sliderStep.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            sliderStep.heightAnchor.constraint(equalToConstant: 35)
+            //sliderStepRating
+            sliderStepRating.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 0),
+            sliderStepRating.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            sliderStepRating.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            sliderStepRating.heightAnchor.constraint(equalToConstant: 35)
         ])
     }
 }
